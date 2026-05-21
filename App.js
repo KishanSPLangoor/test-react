72eac6ef745045e0636302e77348f75ab6183f7e
KishanSPLangoor <166133127+KishanSPLangoor@users.noreply.github.com>
2026-05-21 12:48:41 +0530

    Episode-2

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div",
     { id: "parent" },
     React.createElement("div", { id: "child-1" }, [
-        React.createElement("h1", {}, "I am a child of parent div"),
-        React.createElement("h2", {}, "I am a child of parent div"),
+        React.createElement("h1", {key: "c1-h1" }, "I am a child of parent div"),
+        React.createElement("h2", {key: "c1-h2" }, "I am a 2-child of parent div"),
     ]),
      React.createElement("div", { id: "child-2" }, [
-        React.createElement("h1", {}, "I am a child of parent div"),
-        React.createElement("h2", {}, "I am a child of parent div"),
+        React.createElement("h1", {key: "c2-h1" }, "I am a 3-child of parent div"),
+        React.createElement("h2", {key: "c2-h2" }, "I am a 4-child of parent div"),
     ]),
 );
+console.log(parent); // object
 
-// const heading = React.createElement('h1', {
-//     id: "heading",
-//     xyz: "abc"
-// }, 'Hello, React!');
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # git init
+
+# npx parcel index.html
+
+# npx parcel build index.html - PROD BUILD
+
+# Parcel
+
+Dev Build
+Local Server
+Caching - Faster Builds
+Image Optimization
+Minification
+Bundling
+Compress
+Code Splitting
+Error Handing
